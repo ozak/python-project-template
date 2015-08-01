36d8c6b9f40a27a5671e6e083aec63d6d3b32c56
Ömer Özak <ozak@users.noreply.github.com>
2015-08-01 08:22:05 -0400

    GNU License

--- my_module/metadata.py
+++ my_module/metadata.py
@@ -13,6 +13,6 @@ description = 'It does cool things'
 authors = ['Foo Bar', 'John Doe']
 authors_string = ', '.join(authors)
 emails = ['foobar@example.com', 'johndoe@thisisfake.org']
-license = 'MIT'
+license = 'GNU'
 copyright = '2015 ' + authors_string
 url = 'http://example.com/'
